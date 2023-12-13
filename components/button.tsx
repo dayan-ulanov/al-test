aaee5d2609df3256a6b8fca9245499a2bf3f1f5e
DAYAN <binaryones.gQ@gmail.com>
2023-12-13 11:58:20 +0600

    build: create minimal func for button components

--- components/button.tsx
+++ components/button.tsx
@@ -1,7 +1,15 @@
 "use client";
 
-export const Button = () => {
+interface Props {
+  type: string;
+  state: string;
+  onClick: () => void;
+}
+
+export const Button: React.FC<Props> = ({ type, state, onClick }) => {
   return (
-    <button>button</button>
+    <button onClick={onClick}>
+      button
+    </button>
   )
 }
